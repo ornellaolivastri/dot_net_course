f3b31307f613e2f78120998c91b494985fd86f30
ornellaolivastri <oolivastri@est.frba.utn.edu.ar>
2021-09-30 19:53:14 -0300

    initial projects

---
+++ SlnDemostraciones/frmEjercicio1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace SlnDemostraciones
+{
+    partial class frmEjercicio1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMateria = new System.Windows.Forms.TextBox();
+            this.txtProfesor = new System.Windows.Forms.TextBox();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "lblMateria";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "lblProfesor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "lblFecha";
+            // 
+            // txtMateria
+            // 
+            this.txtMateria.Location = new System.Drawing.Point(214, 64);
+            this.txtMateria.Name = "txtMateria";
+            this.txtMateria.Size = new System.Drawing.Size(118, 20);
+            this.txtMateria.TabIndex = 3;
+            // 
+            // txtProfesor
+            // 
+            this.txtProfesor.Location = new System.Drawing.Point(214, 97);
+            this.txtProfesor.Name = "txtProfesor";
+            this.txtProfesor.Size = new System.Drawing.Size(118, 20);
+            this.txtProfesor.TabIndex = 4;
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(214, 132);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(118, 20);
+            this.txtFecha.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "mostrar mensaje";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(259, 247);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(35, 13);
+            this.lblMensaje.TabIndex = 7;
+            this.lblMensaje.Text = "label4";
+            // 
+            // frm2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.txtProfesor);
+            this.Controls.Add(this.txtMateria);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frm2";
+            this.Text = "frmAlumnos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMateria;
+        private System.Windows.Forms.TextBox txtProfesor;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblMensaje;
+    }
+}
---
+++ SlnDemostraciones/metodos.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace SlnDemostraciones
+{
+    partial class metodos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPruebaUnoVoid_Click = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPruebaUnoVoid_Click
+            // 
+            this.btnPruebaUnoVoid_Click.Location = new System.Drawing.Point(133, 208);
+            this.btnPruebaUnoVoid_Click.Name = "btnPruebaUnoVoid_Click";
+            this.btnPruebaUnoVoid_Click.Size = new System.Drawing.Size(240, 22);
+            this.btnPruebaUnoVoid_Click.TabIndex = 0;
+            this.btnPruebaUnoVoid_Click.Text = "prueba 1 void";
+            this.btnPruebaUnoVoid_Click.UseVisualStyleBackColor = true;
+            this.btnPruebaUnoVoid_Click.Click += new System.EventHandler(this.btnPruebaUnoVoid_Click_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(201, 164);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // metodos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnPruebaUnoVoid_Click);
+            this.Name = "metodos";
+            this.Text = "metodos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPruebaUnoVoid_Click;
+        private System.Windows.Forms.TextBox txtNombre;
+    }
+}
