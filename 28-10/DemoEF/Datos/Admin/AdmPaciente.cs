8573af8c3941d65b42289da22827567175ae9bdf
ornellaolivastri <oolivastri@est.frba.utn.edu.ar>
2021-10-29 12:21:59 -0300

    clase del 28

---
+++ 28-10/DemoEF/Datos/Admin/AdmPaciente.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+using Datos.Data;
+using Datos.Models;
+
+namespace Datos.Admin
+{
+        
+    public static class AdmPaciente
+    {
+        static DbClinicaContext context = new DbClinicaContext();
+        
+        public static List<Paciente> Listar()
+        {
+            
+            return context.Pacientes.ToList(); //retornamos todos los pacientes
+        }
+
+        public static Paciente TraerPorId(int id)
+        {
+            //TODO implementar Listar()
+            return context.Pacientes.Find(id);
+        }
+
+        public static int Insertar(Paciente paciente)
+        {
+            context.Pacientes.Add(paciente);
+            int filasAfectadas = context.SaveChanges();
+            return filasAfectadas;
+        }
+
+        public static int Modificar(Paciente paciente)
+        {
+            Paciente pacienteOrigen = context.Pacientes.Find(paciente);
+
+            if (pacienteOrigen != null)
+            {
+                pacienteOrigen.Nombre = paciente.Nombre;
+                pacienteOrigen.Apellido = paciente.Apellido;
+                pacienteOrigen.fechaNacimiento = paciente.fechaNacimiento;
+                pacienteOrigen.NroHistoriaClinica = paciente.NroHistoriaClinica;
+                pacienteOrigen.MedicoId = paciente.NroHistoriaClinica;
+
+                return context.SaveChanges();
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static int Eliminar(int id)
+        {
+            Paciente pacienteOrigen = context.Pacientes.Find(id);
+
+            if (pacienteOrigen != null)
+            {
+                context.Pacientes.Remove(pacienteOrigen);
+
+                return context.SaveChanges();
+            }
+            else
+            {
+                return 0;
+            }
+
+            
+        }
+    }
+}
---
+++ 28-10/DemoEF/WindowsEF/FrmPaciente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Datos.Models;
+using Datos.Admin;
+
+namespace WindowsEF
+{
+    public partial class FrmPaciente : Form
+    {
+        public FrmPaciente()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPaciente_Load(object sender, EventArgs e)
+        {
+            gridPacientes.DataSource = AdmPaciente.Listar();
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            Paciente paciente = new Paciente()
+            {
+                Nombre = "Lauri"
+                Apellido = "Ylönen";
+                FechaNacimiento 
+
+            }
+        }
+    }
+}
